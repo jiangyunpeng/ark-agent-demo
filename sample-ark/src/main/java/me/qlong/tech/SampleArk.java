344b92c57f5b1a677b641ffb4536d1c6e9571ffa
bairen <bairen@wacai.com>
2018-10-10 13:05:46 +0200

    add sofa-ark-springboot-starter

--- sample-ark/src/main/java/me/qlong/tech/SampleArk.java
+++ sample-ark/src/main/java/me/qlong/tech/SampleArk.java
@@ -4,12 +4,16 @@
  */
 package me.qlong.tech;
 
+import com.alipay.sofa.ark.support.startup.SofaArkBootstrap;
+
+
 /**
  * @author qilong.zql 18/10/10-上午10:33
  */
 public class SampleArk {
 
     public static void main(String[] args) {
+        SofaArkBootstrap.launch(args);
         System.out.println("Hi, I'm a simple ark project.");
         try {
             System.out.println("\"me.qlong.tech.SampleArk is loaded by \"" + SampleArk.class.getClassLoader());
